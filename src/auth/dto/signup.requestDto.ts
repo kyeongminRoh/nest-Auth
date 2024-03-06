e3a07f0d2a042607a7fff92a5a72855626578232
kyeongminRoh <rudalstpa11@nate.com>
2024-03-06 18:33:38 +0900

    Docs: createDto

---
+++ src/auth/dto/signup.requestDto.ts
@@ -0,0 +1,6 @@
+export type CreateSignupDto = {
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+}
---
+++ src/auth/dto/signup.resposneDto.ts
@@ -0,0 +1,5 @@
+export type SignupResDto = {
+    name: string;
+    email: string;
+    phone: string;
+}
